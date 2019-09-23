b5e00b640497552b11d19710c81ec26d720c7613
Tien Nguyen <nqtien.dev@gmail.com>
2019-09-23 10:41:49 +0300

    Add 14.2 to roadmap

--- src/main/java/com/qtdzz/platform/model/RoadmapItem.java
+++ src/main/java/com/qtdzz/platform/model/RoadmapItem.java
@@ -1,18 +1,18 @@
 package com.qtdzz.platform.model;
 
 public class RoadmapItem {
-    private int major;
+    private String major;
     private String gaDate;
     private String endDate;
     private boolean isLTS;
     private String extendedDate;
 
 
-    public int getMajor() {
+    public String getMajor() {
         return major;
     }
 
-    public void setMajor(int major) {
+    public void setMajor(String major) {
         this.major = major;
     }
 
